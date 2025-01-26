ae1c549002b7952e3e485cb6cbce7e35a4fdd85c
Max Peerutin <peerutinm@gmail.com>
2025-01-26 18:36:26 +0000

    Set up coverage reporting with Codecov

--- jest.system.config.js
+++ jest.system.config.js
@@ -2,6 +2,10 @@ module.exports = {
     testEnvironment: 'node', // Or use 'jest-environment-jsdom' for frontend
     testMatch: ['**/tests/system/**/*.js'],  // Adjust based on your test file locations
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],  // Optional setup file
+    collectCoverage: true,  // Enable coverage collection
+    collectCoverageFrom: ["src/**/*.js"],  // Adjust to your source files location
+    coverageReporters: ["json", "lcov", "text", "clover"],  // Formats for output
+    coverageDirectory: "coverage",  // Where the coverage report will be stored
     testTimeout: 30000,  // Set appropriate timeout for Puppeteer
   };
   
